ce099415b436e0f1479cba609c170597c9e1f0b1
soni2261 <61927635+soni2261@users.noreply.github.com>
2024-07-15 11:05:50 -0400

    part 1

---
+++ part1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int * createArray(int size){
+    int* array = new int[size];
+    return array;
+}
+
+void initializeArray(int* array, int size){
+    for (int i = 0; i < size; i++){
+        array[i] = i;
+    }
+}
+
+void printArray(int* array, int size){
+    for (int i = 0; i < size; i++){
+        cout << array[i];
+    }
+}
+
+void deleteArray(int* array){
+    delete[] array;
+}
+
+int main(){
+    int size;
+    cin >> size;
+    cout << "size:" + std::to_string(size) << "\n";
+    int *array = createArray(size);
+    initializeArray(array, size);
+    printArray(array, size);
+    deleteArray(array);
+
+    //cout << "array pointer: " << array;
+    //delete[] array;
+    return 0;
+}
